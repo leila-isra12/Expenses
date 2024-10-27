a405bf9dcdbfc7c886ac9899b55142c486177f98
Khadidjabahfir <145495554+Khadidjabahfir@users.noreply.github.com>
2024-10-27 16:36:35 +0100

    Add files via upload

--- expensemanager.py
+++ expensemanager.py
@@ -1,17 +1,19 @@
+
+
 class ExpenseManager:
     """Handles expense data and calculations."""
-    def __init__(self, cursor, conn):
-        self.cursor = cursor
-        self.conn = conn
-        # Create the table if it doesn't exist
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS expenses (expense TEXT, Price REAL)")
-        self.conn.commit()
+    def __init__(self , cursor , conn ):
+        # let's start by connecting to the database 
+        self.cursor = cursor 
+        self.conn = conn 
+        self.cursor.execute ("CREATE TABLE IF NOT EXISTS expenses ( expense TEXT,Price INTEGER , month INTEGER , day INTEGER , year INTEGER)")
 
-    def add_expense(self, name, price):
+    def add_expense(self, name, price , day , month ,year ):
         """Adds an expense to the list and returns the updated total."""
-        self.cursor.execute("INSERT INTO expenses (expense, Price) VALUES (?, ?)", (name, price))
+        self.cursor.execute ("INSERT INTO expenses (expense , Price , day , month , year ) VALUES (? ,?,?,?,?)", (name,price, day , month , year ))
         self.conn.commit()
-        return self.calculate_total()
+        return self.calculate_total() 
+        
 
     def calculate_total(self):
         """Calculates the total price of all expenses."""
@@ -28,3 +30,13 @@ class ExpenseManager:
         """Deletes an expense from the list based on its name."""
         self.cursor.execute("DELETE FROM expenses WHERE expense = ?", (name,))
         self.conn.commit()
+    def get_by_month (self , month , year ) : 
+        """Get The expenses within the month """
+        self.cursor.execute ("SELECT * from expenses WHERE year = ? AND month = ? " , (year , month))
+        return self.cursor.fetchall()
+    
+    def get_by_year (self , year ) : 
+        """Get the expenses within the year """
+        self.cursor.execute ("SELECT * from expenses WHERE year = ? " , (year,))
+        return self.cursor.fetchall() 
+
